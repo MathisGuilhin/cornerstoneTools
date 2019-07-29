0ac479d0702291615cc46cad009f36d36abd6977
GUILHIN Mathis <GUILHIN@clb.loc>
2019-07-29 14:40:57 +0200

    remove stddev/mean textbox area

--- src/tools/annotation/FreehandMouseTool.js
+++ src/tools/annotation/FreehandMouseTool.js
@@ -619,7 +619,7 @@ export default class FreehandMouseTool extends BaseAnnotationTool {
           moSuffix = ' HU';
         }
 
-        // Create a line of text to display the mean and any units that were specified (i.e. HU)
+        /*// Create a line of text to display the mean and any units that were specified (i.e. HU)
         let meanText = `Mean: ${numbersWithCommas(
           meanStdDev.mean.toFixed(2)
         )}${moSuffix}`;
@@ -627,7 +627,7 @@ export default class FreehandMouseTool extends BaseAnnotationTool {
         let stdDevText = `StdDev: ${numbersWithCommas(
           meanStdDev.stdDev.toFixed(2)
         )}${moSuffix}`;
-
+        
         // If this image has SUV values to display, concatenate them to the text line
         if (meanStdDevSUV && meanStdDevSUV.mean !== undefined) {
           const SUVtext = ' SUV: ';
@@ -641,6 +641,7 @@ export default class FreehandMouseTool extends BaseAnnotationTool {
         // Add these text lines to the array to be displayed in the textbox
         textLines.push(meanText);
         textLines.push(stdDevText);
+        */
       }
 
       // If the area is a sane value, display it
