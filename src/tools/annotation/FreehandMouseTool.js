191ae818b4699ace3b912ea41f1e9f0170785514
GUILHIN Mathis <GUILHIN@clb.loc>
2019-07-29 15:11:47 +0200

    modify loaded measurement

--- src/tools/annotation/FreehandMouseTool.js
+++ src/tools/annotation/FreehandMouseTool.js
@@ -610,40 +610,6 @@ export default class FreehandMouseTool extends BaseAnnotationTool {
       // Define an array to store the rows of text for the textbox
       const textLines = [];
 
-      // If the mean and standard deviation values are present, display them
-      if (meanStdDev && meanStdDev.mean !== undefined) {
-        // If the modality is CT, add HU to denote Hounsfield Units
-        let moSuffix = '';
-
-        if (modality === 'CT') {
-          moSuffix = ' HU';
-        }
-
-        /*// Create a line of text to display the mean and any units that were specified (i.e. HU)
-        let meanText = `Mean: ${numbersWithCommas(
-          meanStdDev.mean.toFixed(2)
-        )}${moSuffix}`;
-        // Create a line of text to display the standard deviation and any units that were specified (i.e. HU)
-        let stdDevText = `StdDev: ${numbersWithCommas(
-          meanStdDev.stdDev.toFixed(2)
-        )}${moSuffix}`;
-        
-        // If this image has SUV values to display, concatenate them to the text line
-        if (meanStdDevSUV && meanStdDevSUV.mean !== undefined) {
-          const SUVtext = ' SUV: ';
-
-          meanText +=
-            SUVtext + numbersWithCommas(meanStdDevSUV.mean.toFixed(2));
-          stdDevText +=
-            SUVtext + numbersWithCommas(meanStdDevSUV.stdDev.toFixed(2));
-        }
-
-        // Add these text lines to the array to be displayed in the textbox
-        textLines.push(meanText);
-        textLines.push(stdDevText);
-        */
-      }
-
       // If the area is a sane value, display it
       if (area) {
         // Determine the area suffix based on the pixel spacing in the image.
@@ -989,10 +955,6 @@ export default class FreehandMouseTool extends BaseAnnotationTool {
    */
   _editMouseDragCallback(evt) {
     const eventData = evt.detail;
-
-    if (!this.options.mouseButtonMask.includes(eventData.buttons)) {
-      return;
-    }
 
     const toolState = getToolState(eventData.element, this.name);
 
